35d5e37b52144de6827cc5161e9949b9f6f62bf2
Jakobsen <jakobsen@Jakobsens-MacBook-Pro.local>
2017-09-09 21:32:53 +0200

    :rocket: Threads view added

---
+++ resources/views/threads/index.blade.php
@@ -0,0 +1,24 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8 col-md-offset-2">
+                <div class="panel panel-default">
+                    <div class="panel-heading">Threads</div>
+
+                    <div class="panel-body">
+                        @foreach ($threads as $thread)
+                            <article>
+                                <h3>{{ $thread->title }}</h3>
+                                <div class="">
+                                   {{ $thread->body }}
+                                </div>
+                            </article>
+                        @endforeach
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
